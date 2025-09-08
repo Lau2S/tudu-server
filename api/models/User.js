304948d8b84fefac57fe764f034e2e69cae0e986
Juanjo200 <131538215+Juanjo200@users.noreply.github.com>
2025-09-07 23:21:49 -0500

    Sign-up basico con validacion y hashing

--- api/models/User.js
+++ api/models/User.js
@@ -1,35 +1,78 @@
 const mongoose = require("mongoose");
-
+const { isEmail, isStrongPassword } = require("validator");
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
 /**
  * User schema definition.
- * 
+ *
  * Represents application users stored in MongoDB.
  * Includes authentication fields and automatic timestamps.
  */
+
+isValidPassword = (password) => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+  return passwordRegex.test(password);
+};
+
 const UserSchema = new mongoose.Schema(
-    {
-        /**
-         * The unique username of the user.
-         * @type {String}
-         * @required
-         */
-        username: { type: String, required: true },
-        /**
-         * The password of the user.
-         * Stored as plain text here, but should be hashed
-         * before saving in a production environment.
-         * @type {String}
-         * @required
-         */
-        password: { type: String, required: true },
+  {
+    /**
+     * The unique username of the user.
+     * @type {String}
+     * @required
+     */
+    username: { type: String, required: true },
+    /**
+     * The password of the user.
+     * Stored as plain text here, but should be hashed
+     * before saving in a production environment.
+     * @type {String}
+     * @required
+     */
+    password: {
+      type: String,
+      required: true,
+      hash: true,
+      validate: [isValidPassword, "Password not strong enough"],
     },
-    {
-        /**
-         * Adds `createdAt` and `updatedAt` timestamp fields automatically.
-         */
-        timestamps: true
-    }
+
+    name: { type: String },
+
+    lastname: { type: String },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Invalid email"],
+    },
+
+    age: { type: Number, min: 13 },
+  },
+  {
+    /**
+     * Adds `createdAt` and `updatedAt` timestamp fields automatically.
+     */
+    timestamps: true,
+  }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
 
 /**
  * Mongoose model for the User collection.
