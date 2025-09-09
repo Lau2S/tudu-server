f9ad72cd48b1b6ff707c43653937926394706224
Juanjo200 <131538215+Juanjo200@users.noreply.github.com>
2025-09-09 18:35:40 -0500

    ajuste pequeño a modelo User

--- api/models/User.js
+++ api/models/User.js
@@ -37,9 +37,9 @@ const UserSchema = new mongoose.Schema(
       validate: [isValidPassword, "Password not strong enough"],
     },
 
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
 
-    lastname: { type: String, required: true },
+    lastName: { type: String, required: true },
 
     email: {
       type: String,
